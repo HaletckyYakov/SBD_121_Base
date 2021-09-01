c921e3caf4d78ee09d340d693fb0e1b71e0d964f
Hale_fp67@itstepru.onmicrosoft.com <Hale_fp67@itstepru.onmicrosoft.com>
2021-09-01 21:04:07 +0300

    for добавил

--- FOR/FOR.cpp
+++ FOR/FOR.cpp
@@ -8,7 +8,7 @@ void main()
 {
 	setlocale(LC_ALL, "Rus");
 	int n; // количество итераций
-	cout << "Введите количество итераций:"; cin >> n;
+	cout << "Введите количество итераций: "; cin >> n;
 	for (
 		int i = 0; 
 		i < n; 
